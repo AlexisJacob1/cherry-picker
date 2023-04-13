c1b260454158ff4f0fa82288366aaddf03d72d0e
Alexis Jacob <alexis.jacob@sincro.fr>
2023-04-13 16:10:34 +0200

    implem

--- src/cherry-picker.ts
+++ src/cherry-picker.ts
@@ -20,16 +20,36 @@ export class CherryPicker {
 		const pullRequestNumber: number | undefined = github.context.payload.pull_request?.number;
 
 		if (pullRequestNumber === undefined) {
-			throw new Error("No pull request number")
+			throw new Error("No pull request number");
 		}
 
-		this.client.rest.pulls.get({			
-			owner: github.context.repo.owner,
-			repo: github.context.repo.repo,
-			pull_number: pullRequestNumber
-		})
-		.then((pullRequest) => {
-			console.log(pullRequest.data.merge_commit_sha);
+		Promise.all([
+			this.client.rest.pulls.get({			
+				owner: github.context.repo.owner,
+				repo: github.context.repo.repo,
+				pull_number: pullRequestNumber
+			}),
+			this.client.rest.repos.get({
+				owner: github.context.repo.owner,
+				repo: github.context.repo.repo,
+			})
+		])
+		.then((response) => {
+			const [pullRequest, repo] = response;
+
+			this.client.rest.pulls.create({
+				owner: github.context.repo.owner,
+				repo: github.context.repo.repo,
+				base: repo.data.default_branch,
+				head: pullRequest.data.head.label,
+				title: `[REPORT] Report ${pullRequest.data.id} to ${repo.data.default_branch}`
+			})
+			.then((createdPullRequest) => {
+				console.log(`New pull request created: ${createdPullRequest.data.id}`);
+			})
+			.catch((error) => {
+				throw new Error(error);
+			})
 		})
 		.catch((error) => {
 			throw new Error(error);
