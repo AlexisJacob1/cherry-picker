ca6bfc2f08396858622e6786f38c8cad86bed7fe
Alexis Jacob <alexis.jacob@sincro.fr>
2023-04-13 15:09:17 +0200

    test sha commit merge

--- src/cherry-picker.ts
+++ src/cherry-picker.ts
@@ -15,6 +15,24 @@ export class CherryPicker {
 
 	cherryPickLastCommitAndReportToDevelop() {
 		console.log("Context payload");
-		console.log(JSON.stringify(github.context.payload.pull_request?.merged, undefined, 4));
+		console.log(JSON.stringify(github.context.payload.pull_request, undefined, 4));
+
+		const pullRequestNumber: number | undefined = github.context.payload.pull_request?.number;
+
+		if (pullRequestNumber === undefined) {
+			throw new Error("No pull request number")
+		}
+
+		this.client.rest.pulls.get({			
+			owner: github.context.repo.owner,
+			repo: github.context.repo.repo,
+			pull_number: pullRequestNumber
+		})
+		.then((pullRequest) => {
+			console.log(pullRequest.data.merge_commit_sha);
+		})
+		.catch((error) => {
+			throw new Error(error);
+		});
 	}
 }
