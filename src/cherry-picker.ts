5a6736e4977748f606c95b0ad01c75a7677c7558
Alexis Jacob <114417622+AlexisJacob1@users.noreply.github.com>
2024-01-11 08:09:56 +0100

    Prod test (#34)
    * bump version
    
    * add log to get fetched pull request content
    
    * rebuild action

--- src/cherry-picker.ts
+++ src/cherry-picker.ts
@@ -1,24 +1,24 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
-import { existsSync, readFileSync, readdirSync } from 'fs';
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import {existsSync, readFileSync, readdirSync} from 'fs';
 
 export class CherryPicker {
-	
 	protected client: ReturnType<typeof github.getOctokit>;
-	
+
 	constructor() {
 		const token: string | undefined = core.getInput('repo-token');
 		if (!token) {
-			throw new Error("Invalid token");
+			throw new Error('Invalid token');
 		}
 		this.client = github.getOctokit(token);
 	}
-	
-	async cherryPickLastCommitAndReportToDevelop() {
-		const pullRequestNumber: number | undefined = github.context.payload.pull_request?.number;
-		
+
+	async cherryPickLastCommitAndReportToDevelop(): Promise<void> {
+		const pullRequestNumber: number | undefined =
+			github.context.payload.pull_request?.number;
+
 		if (pullRequestNumber === undefined) {
-			throw new Error("No pull request number");
+			throw new Error('No pull request number');
 		}
 
 		const pullRequest = await this.client.rest.pulls.get({
@@ -26,11 +26,15 @@ export class CherryPicker {
 			repo: github.context.repo.repo,
 			pull_number: pullRequestNumber
 		});
+		console.log('Pull request fetched successfully');
 
 		const repository = await this.client.rest.repos.get({
 			owner: github.context.repo.owner,
-			repo: github.context.repo.repo,
+			repo: github.context.repo.repo
 		});
+		console.log('Repository fetched successfully');
+
+		console.log('Fetched pull request content', pullRequest);
 
 		const createdPullRequest = await this.client.rest.pulls.create({
 			owner: github.context.repo.owner,
@@ -38,15 +42,20 @@ export class CherryPicker {
 			base: repository.data.default_branch,
 			head: pullRequest.data.head.label,
 			title: `Report #${pullRequest.data.number} to ${repository.data.default_branch}`,
-			body: this.getPullRequestBody(pullRequest.data.number, repository.data.default_branch)
+			body: this.getPullRequestBody(
+				pullRequest.data.number,
+				repository.data.default_branch
+			)
 		});
-		
+		console.log('Pull request created successfully');
+
 		await this.client.rest.issues.addLabels({
 			owner: github.context.repo.owner,
 			repo: github.context.repo.repo,
 			issue_number: createdPullRequest.data.number,
 			labels: ['report-from-prod']
 		});
+		console.log('Labels added successfully');
 
 		await this.client.rest.issues.addAssignees({
 			owner: github.context.repo.owner,
@@ -54,28 +63,34 @@ export class CherryPicker {
 			issue_number: createdPullRequest.data.number,
 			assignees: [github.context.actor]
 		});
+		console.log('Assignees added successfully');
 
-		core.info(`Pull request ${pullRequest.data.title} (#${createdPullRequest.data.number}) created successfully`);
+		console.log(
+			`Pull request ${pullRequest.data.title} (#${createdPullRequest.data.number}) created successfully`
+		);
 	}
 
-	private getPullRequestBody(initialPullRequestNumber: number, defaultBranchName: string): string {
+	private getPullRequestBody(
+		initialPullRequestNumber: number,
+		defaultBranchName: string
+	): string {
 		const message: string[] = [
 			`This is a pull request that was created to report #${initialPullRequestNumber} on ${defaultBranchName}`,
-			"You might need to rebase the pulled branch before merging it"
+			'You might need to rebase the pulled branch before merging it'
 		];
 
-		console.log(readdirSync("./"));
-		if (existsSync("./CODEOWNERS")) {
-			const fileContent = readFileSync("./CODEOWNERS", 'utf-8');
-			fileContent.split(/\r?\n/).forEach(line =>  {
+		console.log(readdirSync('./'));
+		if (existsSync('./CODEOWNERS')) {
+			const fileContent = readFileSync('./CODEOWNERS', 'utf-8');
+			for (const line of fileContent.split(/\r?\n/)) {
 				message.push(line.trim());
-			});
+			}
 		} else {
-			core.info("No CODEOWNERS file found. Skipping...");
+			core.info('No CODEOWNERS file found. Skipping...');
 		}
 
 		message.push(`@${github.context.actor}`);
 
-		return message.join("\r\n")
+		return message.join('\r\n');
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import * as core from '@actions/core'
-import { CherryPicker } from './cherry-picker'
+import * as core from '@actions/core';
+import {CherryPicker} from './cherry-picker';
 
 async function run(): Promise<void> {
 	try {
 		// const ms: string = core.getInput('milliseconds')
 		// core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-		
+
 		// core.debug(new Date().toTimeString())
 		// await wait(parseInt(ms, 10))
 		// core.debug(new Date().toTimeString())
-		
+
 		// core.setOutput('time', new Date().toTimeString())
 		await new CherryPicker().cherryPickLastCommitAndReportToDevelop();
 	} catch (error) {
-		if (error instanceof Error) core.setFailed(error.message)
+		if (error instanceof Error) core.setFailed(error.message);
 	}
 }
 
-run()
+run();
